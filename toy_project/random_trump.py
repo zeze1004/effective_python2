2a98f9afac0645c6345e7d0bbd7548df87bb199b
zeze <thwjd2717@gmail.com>
2023-01-26 22:12:37 +0900

    fix: iterator 클래스에서 카드덱 생성하기

--- toy_project/random_trump.py
+++ toy_project/random_trump.py
@@ -4,6 +4,15 @@ import random
 
 class Counter:
     def __init__(self, start, end):
+        self.card = ['color_joker', 'black_joker']
+        self.shape = ['♠', '♥', '♦', '♣']
+        self.card_num = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'A', 'J', 'Q', 'K']
+        for i in self.shape:
+            for j in self.card_num:
+                self.card.append(i + j)
+
+        random.shuffle(self.card)
+
         self.num = start
         self.end = end
 
@@ -14,33 +23,20 @@ class Counter:
         if self.num > self.end:
             raise StopIteration
         else:
-            print(self.num, card[self.num])
+            print(self.num, self.card[self.num - 1])
             self.num += 1
             return self.num - 1
 
 
 # Driver code
 if __name__ == '__main__':
-    shape = ['♠', '♥', '♦', '♣']
-    num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'A', 'J', 'Q']
-    card = ['color_joker', 'black_joker']
-
-    # 카드덱 만들기
-    for i in shape:
-        for j in num:
-            card.append(i + j)
-
-    random.shuffle(card)
-    # print(card)
-
     # 클래스 호출
     c1 = Counter(1, 54)
     obj = iter(c1)
     try:
-        while True: # Print till error raised
+        while True:  # Print till error raised
             next(obj)
             pass
     except:
         # when StopIteration raised, Print custom message
         print("\nGAME OVER")
-
