147e36d2ff4b4e3bf245a8a2d82bdb4dddffb4ca
zeze <thwjd2717@gmail.com>
2023-02-12 12:29:42 +0900

    refactor: Game 내부의 play()에서 게임 실행하도록 수정

--- toy_project/black_jack_game.py
+++ toy_project/black_jack_game.py
@@ -5,12 +5,10 @@ SUIT = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 FACE = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 VALUES = {face: value for (face, value) in zip(FACE, range(2, 11))}
 VALUES.update({'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11})
-print(VALUES)
 
 
 class CardDeck:
     """카드덱 만들기"""
-
     def __init__(self):
         self.deck = self.__setup_cards()
 
@@ -49,7 +47,7 @@ class Dealer:
 
     def add_card(self, card):
         self.dealer_cards.append(card)
-        self.value += card_value.get_value(card)
+        self.value += self.card_value.get_value(card)
         print("dealer:", self.value)
 
     def own_card(self):
@@ -65,7 +63,7 @@ class Player:
 
     def add_card(self, card):
         self.players_cards.append(card)
-        self.value += card_value.get_value(card)
+        self.value += self.card_value.get_value(card)
         print("player:", self.value)
 
     def own_card(self):
@@ -75,8 +73,7 @@ class Player:
 # player is game이 아니므로 상속 X
 # Game has player 은 ok
 class Game:
-    """player와 dealer에게 덱 할당시키고 loop문으로 게임 플레이"""
-
+    """player와 dealer에게 덱 할당시키고 loop문(play())으로 게임 플레이"""
     def __init__(self, card, player, dealer):  # main 함수의 인스턴스 넘겨받음, 밖에서 선언해야 종속성을 줄일 수 있음 ex. 고스톱 카드로 변경 가능
         self.value = 0
         self.card = card
@@ -86,47 +83,46 @@ class Game:
     def hit(self):
         self.player.add_card(self.card.draw_card())
 
+    # TODO: 처음 stand가 실행 될 때 dealer에게 카드 한 장이 추가로 지급되어야함
     def stand(self):
         self.dealer.add_card(self.card.draw_card())
 
+    def burst(self):
+        if self.player.value > 21:
+            print('player burst!')
+            exit()
+        elif self.dealer.value > 21:
+            print('dealer burst!')
+            exit()
+
+    def setup_play(self):
+        deck = CardDeck()
+        deck.shuffle()
+
+        # 게임 시작시 player에게 두 장의 카드를, 딜러에게는 한 장의 카드를 지급
+        self.player.add_card(deck.draw_card())
+        self.player.add_card(deck.draw_card())
+        self.dealer.add_card(deck.draw_card())
+
     def play(self):
-        while True:
-            x = input("Would you like to Hit or Stand? Enter 'h' or 's' ")
+        x = input("Would you like to Hit or Stand? Enter 'h' or 's' ")
 
-            if x[0].lower() == 'h':
-                Game.hit(self)  # hit() function defined above
+        if x[0].lower() == 'h':
+            Game.hit(self)
+            Game.burst(self)
 
-            elif x[0].lower() == 's':
-                print("Player stands. Dealer is playing.")
-                Game.stand(self)
+        elif x[0].lower() == 's':
+            print("Player stands. Dealer is playing.")
+            Game.stand(self)
+            Game.burst(self)
 
-            else:
-                print("Sorry, please try again.")
-                continue
-            break
+        else:
+            print("Sorry, please try again.")
 
 
 if __name__ == '__main__':
-    # DI <- Dependency Injection
-    deck = CardDeck()
-    deck.shuffle()
-
     card_value = CardValue(FACE, SUIT, VALUES)
-
-    # 카드 두 장 지급
-    player = Player(card_value)
-    dealer = Dealer(card_value)
-    player.add_card(deck.draw_card())
-    dealer.add_card(deck.draw_card())
-
-    game = Game(player=player, card=deck, dealer=dealer)
-    game.hit()
+    game = Game(player=Player(card_value), card=CardDeck(), dealer=Dealer(card_value))
+    game.setup_play()
     while True:
         game.play()
-
-        if player.value > 21:
-            print('player burst!')
-            exit()
-        if dealer.value > 21:
-            print('dealer burst!')
-            exit()
