88f9e102adf1ffeb37da3fac943cf0442fda209a
zeze <thwjd2717@gmail.com>
2023-02-12 11:05:24 +0900

    refactor: 카드 값 구현 구문 수정

--- toy_project/black_jack_game.py
+++ toy_project/black_jack_game.py
@@ -2,8 +2,8 @@ import random
 
 SUIT = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 FACE = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-VALUES = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
-          'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+VALUES = {face: value for (face, value) in zip(FACE, range(2, 11))}
+VALUES.update({'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11})
 
 
 class CardDeck:
